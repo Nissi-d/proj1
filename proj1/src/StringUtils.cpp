d355bb229a95ccbb8e5a50f6ed6cf285d2a4dd5a
Nissi-d <johndoe@email.comnudom@ucdavis.edu>
2025-03-02 14:38:54 -0800

    modified:   proj1/src/StringUtils.cpp

--- proj1/src/StringUtils.cpp
+++ proj1/src/StringUtils.cpp
@@ -135,7 +135,15 @@ std::string Join(const std::string &str, const std::vector< std::string > &vect)
 
 std::string ExpandTabs(const std::string &str, int tabsize) noexcept{
     if (tabsize < 0) return str;
-    
+    if (tabsize == 0){
+        std::string result;
+        for (char ch : str) {
+            if (ch != '\t') {
+                result += ch;
+            }
+        }
+        return result;
+    }
     std::string result;
     int column = 0; 
 
